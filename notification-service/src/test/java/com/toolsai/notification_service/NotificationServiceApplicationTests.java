bf55f0616179f456b42e17302456e681138d9522
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-07-11 19:07:16 +0600

    notification service and recommendation service added

---
+++ notification-service/src/test/java/com/toolsai/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolsai.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
